90149fe94f8cf44c7e14330de8ffc90d2969c945
Viktor Ilkun <ilkun_viktor@mail.ru>
2016-03-04 17:08:18 +0200

    delete

--- src/Assert.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-// вспомогательные ассерты
-// обычный ассерт
-#define Assert(expr) if (!(expr)) { throw exception(); }
-// ассерт с описанием ошибки
-#define Assert2(expr, msg) if (!(expr)) { throw exception(msg); }
